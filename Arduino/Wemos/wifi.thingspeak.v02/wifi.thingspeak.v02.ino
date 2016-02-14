b687d25034156a0c7528cc6910c490c6081ad57a
Jeremy VanDerWal <jjvanderwal@gmail.com>
2016-02-14 18:39:59 +1000

    worked on integrated system

---
+++ Arduino/Wemos/wifi.thingspeak.v02/wifi.thingspeak.v02.ino
@@ -0,0 +1,96 @@
+/*
+  * drafted by Jeremy VanDerWal ( jjvanderwal@gmail.com ... www.jjvanderwal.com )
+  * GNU General Public License .. feel free to use / distribute ... no warranties
+ */
+
+#include <ESP8266WiFi.h>
+#include "DHT.h"
+ 
+#define DHTPIN_0 D3     // what pin we're connected to
+#define DHTTYPE_0 DHT22   // DHT 22  (AM2302)
+
+DHT dht_0(DHTPIN_0, DHTTYPE_0);
+ 
+//WiFi information
+const char* ssid = "NETGEAR18";
+const char* password = "FamilyJADE";
+
+// ThingSpeak Settings
+String APIKey = "H3OJHBYHU101EFYU";             // enter your channel's Write API Key
+const int updateThingSpeakInterval = 20 * 1000; // 20 second interval at which to update ThingSpeak
+
+// function prototypes
+void connectWiFi();
+
+// setup the start
+void setup() {
+  Serial.begin(115200);
+  delay(10);
+  
+  dht_0.begin();
+ 
+}
+
+//the looping script
+void loop() {
+  //confirm the wifi is still connected
+  if (WiFi.status() != WL_CONNECTED) { 
+    connectWiFi();
+  } 
+  
+  //define the strings of sensor values
+  float h = dht_0.readHumidity();
+  float t = dht_0.readTemperature();
+  
+  // Check if any reads failed and exit early (to try again).
+  if (isnan(h) || isnan(t)) {
+    Serial.println("Failed to read from DHT sensor!");
+    return;
+  }
+  
+  String S1 = String(h);
+  String S2 = String(t);
+  String postData = ("field1=" + S1 + "&field2=" + S2);
+  Serial.println(postData);
+  
+  // Update ThingSpeak
+  WiFiClient client; // use WiFiClient class to create TCP connections
+  if (client.connect("api.thingspeak.com", 80)) {
+    client.print("POST /update HTTP/1.1\n");
+    client.print("Host: api.thingspeak.com\n");
+    client.print("Connection: close\n");
+    client.print("X-THINGSPEAKAPIKEY: " + APIKey + "\n");
+    client.print("Content-Type: application/x-www-form-urlencoded\n");
+    client.print("Content-Length: ");
+    client.print(postData.length());
+    client.print("\n\n");
+    client.print(postData);
+    String line = client.readStringUntil('\r');   // read all the lines of the reply from server and print them to Serial
+    Serial.println(line);
+  }
+  client.stop();
+
+  delay(updateThingSpeakInterval); // delay by the interval defined
+   
+}
+
+//section to start the WIFI
+void connectWiFi() {
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  
+  WiFi.begin(ssid, password);
+  
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");  
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+  Serial.println();
+}
+
---
+++ Arduino/sound_trial/sound_trial.ino
@@ -0,0 +1,17 @@
+//these define the pin connections
+const int microphonePin= A0; //the microphone positive terminal will connect to analog pin A0 to be read
+//const int ledPin=13; //the code will flash the LED connected to pin 13
+
+int sample; //the variable that will hold the value read from the microphone each time
+
+//const int threshold= 800;//the microphone threshold sound level at which the LED will turn on
+
+void setup() {
+//  pinMode (ledPin, OUTPUT);//sets digital pin 13 as output
+  Serial.begin(115200); //sets the baud rate at 9600 so we can check the values the microphone is obtaining on the Serial Monitor
+}
+
+void loop(){
+  sample= analogRead(microphonePin); //the arduino takes continuous readings from the microphone
+  Serial.println(sample);
+} 
