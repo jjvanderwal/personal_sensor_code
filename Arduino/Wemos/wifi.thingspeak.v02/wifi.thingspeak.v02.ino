2dd776d9e5d6cc27e186adb51fb6bedc7a1a632f
Jeremy VanDerWal <jjvanderwal@gmail.com>
2016-02-15 20:42:30 +1000

    added baro pressure sensor

--- Arduino/Wemos/wifi.thingspeak.v02/wifi.thingspeak.v02.ino
+++ Arduino/Wemos/wifi.thingspeak.v02/wifi.thingspeak.v02.ino
@@ -6,34 +6,38 @@
 #include <ESP8266WiFi.h>
 #include "DHT.h"
 #include <SparkFunTSL2561.h>
+#include <SFE_BMP180.h>
 #include <Wire.h>
 
 //define and initialize some of the pins/types 
-#define DHTPIN_0 D3     // what pin we're connected to
-#define DHTTYPE_0 DHT22   // DHT 22  (AM2302)
-DHT dht_0(DHTPIN_0, DHTTYPE_0);
-const int microphonePin = A0; //the microphone envelope terminal will connect to analog pin A0 to be read
-SFE_TSL2561 light; // Create an SFE_TSL2561 object, here called "light":
+#define DHTPIN_0 D3               // what pin we're connected to
+#define DHTTYPE_0 DHT22           // DHT 22  (AM2302)
+DHT dht_0(DHTPIN_0, DHTTYPE_0);   //define the DHT sensor as the DHT22
+const int microphonePin = A0;     //the microphone envelope terminal will connect to analog pin A0 to be read
+SFE_TSL2561 light;                // Create an SFE_TSL2561 object, here called "light":
+SFE_BMP180 pressure;              //create an SFE_BMP180 object, here called "pressure"
+#define ALTITUDE 20.0             // Altitude of Townsville in meters
+
  
 //WiFi information
-const char* ssid = "NETGEAR18";
-const char* password = "FamilyJADE";
+const char* ssid = "NETGEAR18";         //set the wifi id
+const char* password = "FamilyJADE";    //define the wifi password
 
 // ThingSpeak Settings
 String APIKey = "H3OJHBYHU101EFYU";             // enter your channel's Write API Key
-const int updateThingSpeakInterval = 50 * 1000; // 20 second interval at which to update ThingSpeak
+const int updateThingSpeakInterval = 50 * 1000; // 50 second interval at which to update ThingSpeak
 
 // function prototypes
 void connectWiFi();
 
 // Variable Setup
-long lastConnectionTime = 0;
-int sound_max = 0; // variable to hold max sound value for each thingspeak iteration
-long sound_count = 0; // variable to hold number of sound samples for each thingspeak iteration
-long sound_sum = 0; // variable to hold sum of the sound value for each thingspeak iteration
-int sound_sample; //the variable that will hold the value read from the microphone each time
-boolean light_gain;     // Gain setting, 0 = X1, 1 = X16;
-unsigned int light_ms;  // Integration ("shutter") time in milliseconds
+long lastConnectionTime = 0;        //define the last connection time so that we can count down until the next thingspeak upload
+int sound_max = 0;                  // variable to hold max sound value for each thingspeak iteration
+long sound_count = 0;               // variable to hold number of sound samples for each thingspeak iteration
+long sound_sum = 0;                 // variable to hold sum of the sound value for each thingspeak iteration
+int sound_sample;                   //the variable that will hold the value read from the microphone each time
+boolean light_gain;                 // Gain setting, 0 = X1, 1 = X16;
+unsigned int light_ms;              // Integration ("shutter") time in milliseconds
 
 // setup the start
 void setup() {
@@ -45,34 +49,31 @@ void setup() {
   
   //start the light sensor
   light.begin(); 
-  // If gain = false (0), device is set to low gain (1X)
-  // If gain = high (1), device is set to high gain (16X)
-  light_gain = 0;
-  // If time = 0, integration will be 13.7ms
-  // If time = 1, integration will be 101ms
-  // If time = 2, integration will be 402ms
-  // If time = 3, use manual start / stop to perform your own integration
-  unsigned char time = 2;
-  // setTiming() will set the third parameter (ms) to the requested integration time in ms (this will be useful later):
-  light.setTiming(light_gain,time,light_ms);
-  // To start taking measurements, power up the sensor:
-  light.setPowerUp();
+  light_gain = 0;                               // If gain = false (0), device is set to low gain (1X)
+                                                // If gain = high (1), device is set to high gain (16X)
+  unsigned char time = 2;                       // If time = 0, integration will be 13.7ms
+                                                // If time = 1, integration will be 101ms
+                                                // If time = 2, integration will be 402ms
+                                                // If time = 3, use manual start / stop to perform your own integration
+  light.setTiming(light_gain,time,light_ms);    // setTiming() will set the third parameter (ms)
+                                                // to the requested integration time in ms (this will be useful later):
+  light.setPowerUp();                           // To start taking measurements, power up the sensor:
   
+  //start the pressure sensor
+  pressure.begin();
 }
 
 //the looping script
 void loop() {
+  
   //confirm the wifi is still connected
-  if (WiFi.status() != WL_CONNECTED) { 
-    delay(10);
-    connectWiFi(); 
-  } 
+  if (WiFi.status() != WL_CONNECTED) { connectWiFi(); }
   
   //track sound information
-  sound_sample= analogRead(microphonePin); //the arduino takes continuous readings from the microphone
+  sound_sample= analogRead(microphonePin);                  //the arduino takes continuous readings from the microphone
   if (sound_max < sound_sample) {sound_max = sound_sample;} //keep the maximum value
-  sound_sum += sound_sample; //sum up the sound values
-  sound_count += 1;
+  sound_sum += sound_sample;                                //sum up the sound values
+  sound_count += 1;                                         //keep track of the number of samples
     
   //run this if the time has hit the updateThingSpeakInterval
   if (millis() - lastConnectionTime > updateThingSpeakInterval) {
@@ -97,32 +98,50 @@ void loop() {
     Serial.println(sound_count);
     String S3 = String(round(sound_sum / sound_count));
     String S4 = String(sound_max);
-    if (postData == "") {
-      postData += ("field3=" + S3 + "&field4=" + S4);
-    } else {
-      postData += ("&field3=" + S3 + "&field4=" + S4);
-    }
-    sound_sum = 0;
-    sound_max = 0;
-    sound_count = 0;
+    if (postData != "") { postData += ("&"); }
+    postData += ("field3=" + S3 + "&field4=" + S4);
+    sound_sum = 0; sound_max = 0; sound_count = 0;      //reset values to 0
     
     //work with the light data
     unsigned int data0, data1;
-    if (light.getData(data0,data1)) { // getData() returned true, communication was successful
-      double lux;    // Resulting lux value
-      boolean good;  // True if neither sensor is saturated
+    if (light.getData(data0,data1)) {                           // getData() returned true, communication was successful
+      double lux;                                               // Resulting lux value
+      boolean good;                                             // True if neither sensor is saturated
       good = light.getLux(light_gain,light_ms,data0,data1,lux); // Perform lux calculation:
       if (good) {
         String S5 = String(lux);
-        if (postData == "") {
-          postData += ("field5=" + S5);
-        } else {
-          postData += ("&field5=" + S5);
-        }      
+        if (postData != "") { postData += ("&"); }
+        postData += ("field5=" + S5);
       } 
     }    
     
-    Serial.println(postData);
+    //work with the pressure data
+    char status;
+    double T,P,p0;
+    status = pressure.startTemperature();
+    if (status != 0) {
+      delay(status);                            // Wait for the measurement to complete:
+      status = pressure.getTemperature(T);      // Retrieve the completed temperature measurement stored in the variable T.
+      if (status != 0) {                        // Function returns 1 if successful, 0 if failure.
+        status = pressure.startPressure(3);     // Start a pressure measurement:
+                                                // The parameter is the oversampling setting, from 0 to 3 (highest res, longest wait).
+                                                // If request is successful, the number of ms to wait is returned.
+                                                // If request is unsuccessful, 0 is returned.
+        if (status != 0) {
+          delay(status);                        // Wait for the measurement to complete:
+          status = pressure.getPressure(P,T);   // Retrieve the completed pressure measurement: the measurement is stored in the variable P.
+          if (status != 0) {                    // Function returns 1 if successful, 0 if failure.
+            p0 = pressure.sealevel(P,ALTITUDE); // p0 = sea-level compensated pressure in mb
+            String S6 = String(p0);
+            String S7 = String(T);
+            if (postData != "") { postData += ("&"); }
+            postData += ("field6=" + S6 + "&field7=" + S7);
+          }
+        }
+      }
+    }
+   
+    Serial.println(postData);    //print the post data to the serial port
     
     // Update ThingSpeak
     WiFiClient client; // use WiFiClient class to create TCP connections
@@ -148,6 +167,7 @@ void loop() {
 
 //section to start the WIFI
 void connectWiFi() {
+  delay(10);
   Serial.println();
   Serial.print("Connecting to ");
   Serial.println(ssid);
